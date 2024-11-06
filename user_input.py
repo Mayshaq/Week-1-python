c7063f65708c6574900d03216f17b1f311268178
Meshark Kemei <mesharkkip@gmail.com>
2024-11-06 20:03:59 +0300

    completed

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where do you live? ")
+
+print(f"Hello, {name}! It's great to meet you. You're {age} years old and live in {location}.")
